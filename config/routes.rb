3d9114072348806b2d815bafd2d1503304a54fb1
Raja Guha <sumit@anaraja.com>
2010-07-04 10:52:09 -0700

    added routes, removed default index.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 ActionController::Routing::Routes.draw do |map|
+  map.contact '/contact', :controller => 'pages', :action => 'contact'
+  map.about   '/about',   :controller => 'pages', :action => 'about'
+  map.help    '/help',    :controller => 'pages', :action => 'help'
+  
   map.resources :microposts
 
   map.resources :users
@@ -36,6 +40,7 @@ ActionController::Routing::Routes.draw do |map|
 
   # You can have the root of your site routed with map.root -- just remember to delete public/index.html.
   # map.root :controller => "welcome"
+  map.root :controller => 'pages', :action => 'home'
 
   # See how all your routes lay out with "rake routes"
 
---
+++ spec/integration/layout_links_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe "Layout links" do
+  it "should have a Home page at'/'" do
+    get '/'
+    response.should render_template('pages/home')
+  end
+  
+  it "should have a Contact page at '/contact'" do
+    get '/contact'
+    response.should render_template('pages/contact')
+  end
+  
+  it "should have a Help page at '/help'" do
+    get '/help'
+    response.should render_template('pages/help')
+  end  
+end
