6e9b56a373ed1e85d16dcb6f3a3e7386bae7b423
Raja Guha <sumit@anaraja.com>
2010-07-05 19:48:34 -0700

    user show action

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :users
+  map.resources :microposts
+
   map.contact '/contact', :controller => 'pages', :action => 'contact'
   map.about   '/about',   :controller => 'pages', :action => 'about'
   map.help    '/help',    :controller => 'pages', :action => 'help'
   map.signup  '/signup',  :controller => 'users', :action => 'new'
   
-  map.resources :microposts
-
-  map.resources :users
-
   # The priority is based upon order of creation: first created -> highest priority.
 
   # Sample of regular route:
