e9d98b483da266b740c231bb8f8f23223f035c52
Raja Guha <sumit@anaraja.com>
2010-07-08 22:56:11 -0700

    user model with passwords, and user phow page

---
+++ spec/factories.rb
@@ -0,0 +1,7 @@
+# By using the symbol ':user', we get Factory Girl to simulate the user model
+Factory.define :user do |user|
+  user.name                   "Sumit Guha"
+  user.email                  "sguha@yahoo.com"
+  user.password               "foobar"
+  user.password_confirmation  "foobar"
+end
